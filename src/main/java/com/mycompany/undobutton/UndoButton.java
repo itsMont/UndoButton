586acfea53b30d70391b81cc6b27e13c2d3e3eb1
itsMont <jmontanaa@unal.edu.co>
2022-08-29 12:36:59 -0500

    Cambio de colores y texto

---
+++ src/main/java/com/mycompany/undobutton/UndoButton.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.undobutton;
+import View.GeneralWindow;
+/**
+ *
+ * @author jhojan
+ */
+public class UndoButton {
+
+    public static void main(String[] args) {
+        new GeneralWindow().setVisible(true);
+    }
+}
